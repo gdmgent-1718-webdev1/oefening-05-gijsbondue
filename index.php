e36c0451591b6dbd12cc6cacfe623957f3e970bc
gijs bondue <gijsbondue@MacBook-Pro-van-gijs.local>
2017-10-15 22:22:59 +0200

    <!DOCTYPE html> <html lang="en">   <head>     <title>oefening 5</title>     <meta charset="UTF-8">     <meta name="viewport" content="width=device-width, initial-scale=1">   </head>   <body>   <?php     print "flabberflabber";     function checkLeeftijd($leeftijd){       if( $leeftijd < 17 ){         return "<p>${leeftijd} jaar is te jong voor deze film.</p>";         }       elseif( $leeftijd < 19 ){         return "<p>${leeftijd} jaar, je hebt ouderlijk toezicht nodig.</p>";         }       else{         return "<p>Je bent al ${leeftijd} jaar. Geniet van de film!</p>";       }     }       for($i = 0;$i<99; $i++){         print checkLeeftijd(rand(0, 50));       }
        ?>
      </body>
    </html>

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>oefening 5</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+  <?php
+    print "flabberflabber";
+    function checkLeeftijd($leeftijd){
+      if( $leeftijd < 17 ){
+        return "<p>${leeftijd} jaar is te jong voor deze film.</p>";
+        }
+      elseif( $leeftijd < 19 ){
+        return "<p>${leeftijd} jaar, je hebt ouderlijk toezicht nodig.</p>";
+        }
+      else{
+        return "<p>Je bent al ${leeftijd} jaar. Geniet van de film!</p>";
+      } 
+    }
+      for($i = 0;$i<99; $i++){
+        print checkLeeftijd(rand(0, 50));
+      }
+    
+    
+
+    ?>
+  </body>
+</html>
